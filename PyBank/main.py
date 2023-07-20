89a65743ce0452c6ecac9c9062c58a53ff66b4a5
Elevate04 <carlosv4572@gmail.com>
2023-07-19 22:48:27 -0400

    Uploading my Python homework assignment

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,58 @@
+import os
+import csv
 
+budget_data = os.path.join("PyBank", "Resources", "budget_data.csv")
+
+# Create empty lists to iterate through specific rows for the following variables
+total_months = []
+total_profit = []
+monthly_profit_change = []
+
+Net_Total = []
+
+Average_Change = []
+
+with open (budget_data) as csv_file:
+    csv_reader = csv.reader(csv_file)
+    
+    next(csv_reader)
+    
+    for row in csv_reader:
+        
+        total_months.append(row[0])
+        total_profit.append(int(row[1]))
+        
+        
+    for x in range(len(total_profit)-1):
+        monthly_profit_change.append(total_profit[x+1] - total_profit[x])
+Net_Total.append(sum(total_profit))
+Max_Value = max(monthly_profit_change)
+Min_Value = min(monthly_profit_change)
+Max_Month = monthly_profit_change.index(max(monthly_profit_change))
+Min_Month = monthly_profit_change.index(min(monthly_profit_change))
+Average_Change.append(round(sum(monthly_profit_change)/len(monthly_profit_change)))
+
+print('Financial Analysis')
+print('-------------------')
+print(f'Total Months:{len(total_months)}')
+print(f'Total:${sum(Net_Total)}')
+print(f'Average Change {(sum(Average_Change))}')
+print(f"Greatest Increase in Profits: {(total_months[Max_Month+1])} ${Max_Value}")
+print(f'Greatest Decrease in Profits: {(total_months[Min_Month+1])} ${Min_Value}')
+
+Output_file = os.path.join("PyBank", "Resources", "Financial Analysis Text")
+
+with open(Output_file, "w") as file:
+    file.write("Financial Analysis")
+    file.write("\n")
+    file.write("-----------------")
+    file.write("\n")
+    file.write(f"Total Months: {(len(total_months))}")
+    file.write("\n")
+    file.write(f"Total:${sum(Net_Total)}")
+    file.write("\n")
+    file.write(f'Average Change {(sum(Average_Change))}')
+    file.write("\n")
+    file.write(f"Greatest Increase in Profits: {(total_months[Max_Month+1])} ${Max_Value}")
+    file.write("\n")
+    file.write(f"Greatest Decrease in Profits: {(total_months[Min_Month+1])} ${Min_Value}")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,77 @@
+import os
+import csv
 
+election_data = os.path.join("PyPoll", "Resources", "election_data.csv")
+
+Total_Votes = []
+Candidates = []
+Charles_Casper_Stockham_Vote_Total = []
+Charles = 0
+Diana_DeGette_Vote_Total = []
+Diana = 0
+Raymon_Anthony_Doane_Vote_Total = []
+Raymon = 0
+
+
+with open(election_data) as csv_file:
+    csv_reader = csv.reader(csv_file)
+    
+    next(csv_reader)
+    
+    for row in csv_reader:
+        Total_Votes.append(row[0])
+        Candidates.append(row[2])
+        
+        if (row[2]) == "Charles Casper Stockham":
+            Charles = Charles + 1
+        elif (row[2]) == "Diana DeGette":
+            Diana = Diana + 1
+        else:
+            Raymon = Raymon + 1
+        
+    Charles_Casper_Stockham_Vote_Total = (Charles/len(Total_Votes)) * 100
+    Charles_Casper_Stockham_Vote_Total = round(Charles_Casper_Stockham_Vote_Total, 3)
+    
+    Diana_DeGette_Vote_Total = (Diana/len(Total_Votes)) * 100
+    Diana_DeGette_Vote_Total = round(Diana_DeGette_Vote_Total, 3)
+    
+    Raymon_Anthony_Doane_Vote_Total = (Raymon/len(Total_Votes) * 100)
+    Raymon_Anthony_Doane_Vote_Total = round(Raymon_Anthony_Doane_Vote_Total, 3)
+    
+    if Charles > Diana and Charles > Raymon:
+        Winner = "Winner: Charles Casper Stockham"
+    elif Diana > Charles and Diana > Raymon:
+        Winner = "Winner: Diana DeGette"
+    else:
+        Winner = "Winner: Raymon Anthony Doane"
+        
+    
+    print("Election Results")
+    print("-----------------")
+    print(f"Total Votes: {(len(Total_Votes))}")
+    print("-----------------")
+    print(f"Charles Casper Stockham:{Charles_Casper_Stockham_Vote_Total}% {Charles}")
+    print(f"Diana DeGette:{Diana_DeGette_Vote_Total}% {Diana}")
+    print(f"Raymon Anthony Doane:{Raymon_Anthony_Doane_Vote_Total}% {Raymon}")
+    print("-----------------")
+    print(Winner)
+    
+output_file = os.path.join("PyPoll", "Resources", "election_data_text")
+
+with open (output_file, "w") as file:
+    file.write("Election Results")
+    file.write("\n")
+    file.write("----------------")
+    file.write("\n")
+    file.write(f"Total Votes: {(len(Total_Votes))}")
+    file.write("\n")
+    file.write(f"Charles Casper Stockham:{Charles_Casper_Stockham_Vote_Total}% {Charles}")
+    file.write("\n")
+    file.write(f"Diana DeGette:{Diana_DeGette_Vote_Total}% {Diana}")
+    file.write("\n")
+    file.write(f"Raymon Anthony Doane:{Raymon_Anthony_Doane_Vote_Total}% {Raymon}")
+    file.write("\n")
+    file.write("----------------")
+    file.write("\n")
+    file.write(Winner)
+    
